ddaa7ace1fa37cc38395b1af287bbf10aca5bbe9
mostafamostafa01274544-droid <mostafamostafa01274544@gmail.com>
2026-05-17 18:26:38 +0300

    Delete lib directory

--- lib/core/usecases/usecase.dart
+++
@@ -1,7 +0,0 @@
-abstract interface class UseCase<T, P> {
-  Future<T> call(P params);
-}
-
-final class NoParams {
-  const NoParams();
-}
--- lib/domain/repositories/converter_repository.dart
+++
@@ -1,9 +0,0 @@
-import '../entities/conversion_result.dart';
-
-abstract interface class ConverterRepository {
-  Future<ConversionResult> convert({
-    required String inputPath,
-    required String outputPath,
-    required String outputName,
-  });
-}
